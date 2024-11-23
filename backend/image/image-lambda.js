acf891483cf6fc3eeda3d3aa436f44a52dc1f254
mmheemskerk <michael.m.heemskerk@gmail.com>
2024-11-24 00:42:43 +0100

    fix image lambda

--- backend/image/image-lambda.js
+++ backend/image/image-lambda.js
@@ -1,10 +1,23 @@
-import { getSignedUrl } from "@aws-sdk/client-s3";
+// import https from "node:https";
+
+// import { XMLParser } from "fast-xml-parser";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { fromIni } from "@aws-sdk/credential-providers";
+// import { HttpRequest } from "@smithy/protocol-http";
+import {
+  getSignedUrl,
+  S3RequestPresigner,
+} from "@aws-sdk/s3-request-presigner";
+// import { parseUrl } from "@smithy/url-parser";
+// import { formatUrl } from "@aws-sdk/util-format-url";
+// import { Hash } from "@smithy/hash-node";
 
 // TODO MAKE https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/javascript_s3_code_examples.html
 
 const bucketName = "kiikiiworld-serverless-prd-image";
+const region = "eu-central-1";
 
-exports.handler = async (event) => {
+export const handler = async (event) => {
   console.log("event: ", event);
 
   if (event.httpMethod === "GET") {
@@ -18,19 +31,49 @@ exports.handler = async (event) => {
       );
     }
 
-    const params = {
-      Bucket: bucketName,
-      Key: fileName,
-      Expires: 60, // URL validity in seconds
-      ContentType: fileType,
-    };
+    const clientUrl = await createPresignedUrlWithClient({
+      bucket: bucketName,
+      region,
+      fileName,
+      fileType,
+    });
 
-    const presignedUrl = await getSignedUrl("putObject", params);
+    return generateResponse(clientUrl);
 
-    return generateResponse(presignedUrl);
+    // await put(clientUrl, "Hello World");
+
+    // const params = {
+    //   Bucket: bucketName,
+    //   Key: fileName,
+    //   Expires: 60, // URL validity in seconds
+    //   ContentType: fileType,
+    // };
+
+    // const presignedUrl = await getSignedUrl("putObject", params);
+
+    // return generateResponse(presignedUrl);
+  }
+
+  if (event.httpMethod === "POST") {
+    return generateResponse("POST MESSAGE");
   }
 
   return generateResponse("SKIPPED");
+};
+
+const createPresignedUrlWithClient = ({
+  region,
+  bucket,
+  fileName,
+  fileType,
+}) => {
+  const client = new S3Client({ region });
+  const command = new PutObjectCommand({
+    Bucket: bucket,
+    Key: fileName,
+    ContentType: fileType,
+  });
+  return getSignedUrl(client, command, { expiresIn: 3600 });
 };
 
 function generateResponse(responseMessage, statusCode = 200) {
